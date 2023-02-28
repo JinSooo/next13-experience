5aec7805d2273c91ee857474f6a5efce719477c6
JinSo <kimjinso@qq.com>
2023-02-28 10:58:29 +0800

    fix: fetch error

--- app/api/sentence/route.ts
+++ app/api/sentence/route.ts
@@ -7,13 +7,21 @@ function sleep(time: number) {
 }
 
 export async function GET(request: Request) {
-	const res = await fetch('https://api.apiopen.top/api/sentences')
-	const data = await res.json()
+	// const res = await fetch('https://api.apiopen.top/api/sentences')
+	// const data = await res.json()
 
 	// 延迟返回，测试注水
 	await sleep(3000)
 	await sleep(3000)
 	await sleep(3000)
 
-	return NextResponse.json(data)
+	// return NextResponse.json(data)
+	return NextResponse.json({
+		code: 200,
+		message: '成功!',
+		result: {
+			name: '海棠未雨，梨花先雪，一半春休。',
+			from: '王雱《眼儿媚·杨柳丝丝弄轻柔》',
+		},
+	})
 }
